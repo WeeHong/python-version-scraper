513c8835c35af7b52856f093dd54b05999d36dfe
root <root@Ayden.localdomain>
2022-02-04 10:48:45 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+
+	"github.com/gocolly/colly/v2"
+	"github.com/gorilla/mux"
+	"github.com/hashicorp/go-version"
+	"github.com/throttled/throttled"
+	"github.com/throttled/throttled/store/memstore"
+)
+
+func main() {
+	store, err := memstore.New(65536)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	quota := throttled.RateQuota{
+		MaxRate:  throttled.PerMin(20),
+		MaxBurst: 5,
+	}
+
+	rateLimiter, err := throttled.NewGCRARateLimiter(store, quota)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	httpRateLimiter := throttled.HTTPRateLimiter{
+		RateLimiter: rateLimiter,
+		VaryBy:      &throttled.VaryBy{Path: true},
+	}
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/python-stable", func(rw http.ResponseWriter, r *http.Request) {
+		res, err := getStableVersion()
+		if err != nil {
+			log.Fatal(err)
+		}
+		rw.WriteHeader(http.StatusOK)
+		rw.Write([]byte(res))
+	}).Methods("GET")
+
+	router.HandleFunc("/python-prereleases", func(rw http.ResponseWriter, r *http.Request) {
+		rw.WriteHeader(http.StatusOK)
+		rw.Write([]byte("Under Implementation"))
+	}).Methods("GET")
+
+	http.ListenAndServe(":8080", httpRateLimiter.RateLimit(router))
+}
+
+func getStableVersion() (string, error) {
+	v, err := version.NewVersion("0")
+	if err != nil {
+		return "", errors.New("Latest version: " + err.Error())
+	}
+
+	c := colly.NewCollector()
+
+	c.OnHTML(".col-row.two-col .column:first-child a[href]", func(e *colly.HTMLElement) {
+		v = versionChecker(v, e.Attr("href"))
+	})
+
+	// c.Visit("https://www.python.org/ftp/python/")
+	c.Visit("https://www.python.org/downloads/source/")
+
+	return v.String(), nil
+}
+
+func versionChecker(v *version.Version, cur string) *version.Version {
+	regex := regexp.MustCompile(`\d+(\.\d+)+`)
+	matched := regex.FindAllString(cur, -1)
+
+	if len(matched) > 0 {
+		fmt.Println(matched[0])
+
+		current, err := version.NewVersion(matched[0])
+		if err != nil {
+			panic(err.Error())
+		}
+		if v.LessThan(current) {
+			v = current
+		}
+	}
+
+	return v
+}
